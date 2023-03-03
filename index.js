3241425a1bbaab390dd395bc1ae6268bd84a7e1b
Namhyun <nhkz1005@gmail.com>
2023-03-03 13:47:42 +1300

    Make input values retrieved on click

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let hexValue = document.getElementById('seed-value').value
-let colorScheme = document.getElementById('color-schemes').value
-
-fetch(`thecolorapi.com/scheme?hex=${hexValue}&mode=${colorScheme}`)
-  .then(res => res.json())
-  .then(data => console.log(data))
+document.getElementById('get-btn').addEventListener('click', function(){
+  let hexValue = document.getElementById('seed-color').value
+  let colorScheme = document.getElementById('color-schemes').value
+  fetch(`https://thecolorapi.com/scheme?hex=${hexValue}&mode=${colorScheme}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+})
