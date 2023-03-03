8507430a45a3805cabd6bbe3067375c4a806fd77
Namhyun <nhkz1005@gmail.com>
2023-03-03 16:03:20 +1300

    fetch api data

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-document.getElementById('get-btn').addEventListener('click', function(){
-  let hexValue = document.getElementById('seed-color').value
+document.getElementById('get-btn').addEventListener('click', function() {
+  let hexValue = document.getElementById('seed-color').value.toUpperCase().substring(1);
+  
   let colorScheme = document.getElementById('color-schemes').value
-  fetch(`https://thecolorapi.com/scheme?hex=${hexValue}&mode=${colorScheme}`)
+
+  fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}`, 
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    
     .then(res => res.json())
     .then(data => console.log(data))
+  
 })
+
+function getHex(hexValue){
+  return hexValue.toUpperCase().substring(1)
+}
