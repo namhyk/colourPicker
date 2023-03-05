f9dc63f94d52a11f6cd8928be247c75f81729824
Namhyun <nhkz1005@gmail.com>
2023-03-05 15:44:17 +1300

    Load selected colours to DOM

--- index.js
+++ index.js
@@ -3,19 +3,14 @@ document.getElementById('get-btn').addEventListener('click', function() {
   
   let colorScheme = document.getElementById('color-schemes').value
 
-  fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}`, 
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    
+  fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}&mode=${colorScheme}`)   
     .then(res => res.json())
-    .then(data => console.log(data))
-  
+    .then(data => {
+      console.log(data)
+      document.getElementById('color-1').style.backgroundColor = data.colors[0].hex.value
+      document.getElementById('color-2').style.backgroundColor = data.colors[1].hex.value
+      document.getElementById('color-3').style.backgroundColor = data.colors[2].hex.value
+      document.getElementById('color-4').style.backgroundColor = data.colors[3].hex.value
+      document.getElementById('color-5').style.backgroundColor = data.colors[4].hex.value
+    })
 })
-
-function getHex(hexValue){
-  return hexValue.toUpperCase().substring(1)
-}
