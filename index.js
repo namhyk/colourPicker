ee35f0e10e3a862c5c295e15805fb52fa509ca0c
Namhyun <nhkz1005@gmail.com>
2023-03-12 14:05:31 +1300

    style footer

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ document.getElementById('get-btn').addEventListener('click', function() {
       document.getElementById('color-3').style.backgroundColor = data.colors[2].hex.value
       document.getElementById('color-4').style.backgroundColor = data.colors[3].hex.value
       document.getElementById('color-5').style.backgroundColor = data.colors[4].hex.value
+      document.getElementById('hexCode1').innerText = data.colors[0].hex.value
+      document.getElementById('hexCode2').innerText = data.colors[1].hex.value
+      document.getElementById('hexCode3').innerText = data.colors[2].hex.value
+      document.getElementById('hexCode4').innerText = data.colors[3].hex.value
+      document.getElementById('hexCode5').innerText = data.colors[4].hex.value
     })
 })
